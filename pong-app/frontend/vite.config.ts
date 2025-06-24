9395f55a0ad5a63915186174602aadb6112ded87
Teemu Tero <ttero@c1r1p11.hive.fi>
2025-06-24 15:57:35 +0300

    added docker stuff

---
+++ pong-app/frontend/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      'three': path.resolve(__dirname, 'node_modules/three')
+    }
+  }
+});
