6f7278f31f07c0174c5c8f9fd511487fdbea58c8
Waleed Alnaimi <walnaimi@c2r1p3.hive.fi>
2025-07-11 13:50:58 +0300

    added play as a guest

--- pong-app/frontend/src/components/menu.tsx
+++ pong-app/frontend/src/components/menu.tsx
@@ -8,7 +8,7 @@ interface MenuProps {
 const Menu: React.FC<MenuProps> = ({ onPlay }) => {
   return (
     <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-80 z-10">
-      <button onClick={onPlay} className="px-4 py-2 m-2 bg-white text-black font-bold rounded">Play</button>
+      <button onClick={onPlay} className="px-4 py-2 m-2 bg-white text-black font-bold rounded">Play as guest</button>
 
       <Link
         to="/register"
