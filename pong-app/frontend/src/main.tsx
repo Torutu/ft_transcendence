a32d64ff0589df554fbe96e695c7016ad2ce1af9
senthilpoo10 <senthilpoo10@gmail.com>
2025-07-28 21:50:40 +0300

    Update main.tsx

--- pong-app/frontend/src/main.tsx
+++ pong-app/frontend/src/main.tsx
@@ -4,10 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import './styles.css';
 import App from './app';
 
-console.log("React is mounting..."); // Add this line
-let x = localStorage.getItem('token');
-console.log('token:', x, 'isAuthenticated:', !!x); // Log the token and authentication status
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
